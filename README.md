e0f0b3f7e0f438dd5eddb801635a92609d4ef585
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-06-11 09:45:58 -0400

    Added visual test scripts

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# skyux-builder-config
+# SKY UX Builder Config
+
 Configuration files for SKY UX Builder when running on different platforms.
+
+## Visual Testing
+
+### Environment variables needed
+
+#### Visual Failures
+- `VISUAL_FAILURES_ACCESS_TOKEN` An access token created by [blackbaud/skyux-visualtest-results](https://github.com/blackbaud/skyux-visualtest-results) repo owners. This token is provided to each SPA on-request.
+
+#### Visual Baselines
+- `VISUAL_BASELINES_ACCESS_TOKEN` An access token created by the SPA repo owners.
---
+++ scripts/utils.js
@@ -0,0 +1,62 @@
+/* jshint node: true */
+'use strict';
+
+const spawn = require('cross-spawn');
+
+function dirHasChanges(dir) {
+  return exec('git', ['status', dir, '--porcelain'])
+    .then((result) => {
+      // Untracked files are prefixed with '??'
+      // https://git-scm.com/docs/git-status/1.8.1#_output
+      // https://stackoverflow.com/a/6978402/6178885
+      return (result.output && result.output.indexOf('??') > -1);
+    });
+}
+
+function exec(cmd, args, opts) {
+  const cp = spawn(cmd, args, opts);
+
+  return new Promise((resolve, reject) => {
+    let output;
+    cp.stdout.on('data', (data) => {
+      output = data.toString('utf8');
+    });
+
+    let error;
+    cp.stderr.on('data', (data) => {
+      error = data.toString('utf8');
+    });
+
+    cp.on('error', (err) => {
+      reject(err);
+    });
+
+    cp.on('exit', (code) => {
+      if (code === 0) {
+        resolve(output);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+// Get name of feature branch.
+// https://stackoverflow.com/a/12142066/6178885
+function getFeatureBranch() {
+  return exec('git', ['rev-parse', '--abbrev-ref', 'HEAD'])
+    .then((output) => output.replace(/\n/g, ''));
+}
+
+// Get git origin URL.
+function getOriginUrl() {
+  return exec('git', ['config', '--get', 'remote.origin.url'])
+    .then((output) => output.replace(/\n/g, ''));
+}
+
+module.exports = {
+  dirHasChanges,
+  exec,
+  getFeatureBranch,
+  getOriginUrl
+};
