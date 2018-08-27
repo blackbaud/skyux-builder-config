84edec6de8710ecdf4ade4c9d328454ba0eacec4
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-08-27 11:57:49 -0400

    Removed browserstack from protractor

--- travis/config/protractor/protractor.conf.js
+++ travis/config/protractor/protractor.conf.js
@@ -3,11 +3,6 @@
 
 const getConfig = require('../../../shared/protractor/shared.protractor.conf');
 
-const config = getConfig({
-  BROWSER_STACK_USERNAME: process.env.BROWSER_STACK_USERNAME,
-  BROWSER_STACK_ACCESS_KEY: process.env.BROWSER_STACK_ACCESS_KEY,
-  BROWSER_STACK_BUILD_ID: process.env.TRAVIS_BUILD_NUMBER,
-  BROWSER_STACK_PROJECT: 'SKY UX Travis CI: Protractor'
-});
+const config = getConfig();
 
 exports.config = config;
